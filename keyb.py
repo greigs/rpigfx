5f7bd7a338cd54d0377dd2afddad955ec25400ba
greig <gstewart@streamba.net>
2018-03-26 23:25:33 +0100

    wip

--- keyb.py
+++ keyb.py
@@ -281,7 +281,7 @@ for iconPathLocal in keysets:
 # Main loop ----------------------------------------------------------------
 framecount = 0
 # Desired framerate in frames per second. Try out other values.              
-FPS = 5
+FPS = 1
 # How many seconds the "game" is played.
 playtime = 0.0
 loadset = False
